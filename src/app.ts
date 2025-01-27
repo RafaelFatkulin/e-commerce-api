6404a9a890d1a96b65e2655f91a3b59f643dfbf1
RafaelFatkulin <armistman@vk.com>
2025-01-27 23:46:29 +0500

    fix cors

--- src/app.ts
+++ src/app.ts
@@ -1,41 +1,26 @@
-import { configureOpenAPI } from '@lib/configure-open-api'
-import { createApp } from '@lib/create-app'
-import { auth } from '@modules/auth'
-import { brand } from '@modules/brand'
-import { category } from '@modules/category'
-import { core } from '@modules/core'
-import { user } from '@modules/user'
-import { serveStatic } from 'hono/bun'
-import { cors } from 'hono/cors'
+import { configureOpenAPI } from "@lib/configure-open-api";
+import { createApp } from "@lib/create-app";
+import { auth } from "@modules/auth";
+import { brand } from "@modules/brand";
+import { category } from "@modules/category";
+import { core } from "@modules/core";
+import { user } from "@modules/user";
+import { serveStatic } from "hono/bun";
+import { cors } from "hono/cors";
 
-export const app = createApp()
+export const app = createApp();
 
-app.use(
-  '*',
-  cors({
-    origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
-    allowMethods: ['POST', 'GET', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    credentials: true,
-  }),
-)
+const routes = [core, user, auth, category, brand];
 
-const routes = [
-  core,
-  user,
-  auth,
-  category,
-  brand,
-]
-
-configureOpenAPI(app)
+configureOpenAPI(app);
 
 routes.forEach((route) => {
-  app.route('/', route)
-})
+  app.route("/", route);
+});
 
-app.use('/static/*', serveStatic({ root: './' }))
+app.use("/static/*", serveStatic({ root: "./" }));
 
-app.openAPIRegistry.registerComponent('securitySchemes', 'Bearer', {
-  type: 'http',
-  scheme: 'bearer',
-})
+app.openAPIRegistry.registerComponent("securitySchemes", "Bearer", {
+  type: "http",
+  scheme: "bearer",
+});
--- src/lib/create-app.ts
+++ src/lib/create-app.ts
@@ -1,55 +1,68 @@
-import type { AppBindings } from './types'
-import { logger } from '@helpers/logger'
-import { OpenAPIHono } from '@hono/zod-openapi'
-import { createErrorResponse } from '@utils/response'
-import { cors } from 'hono/cors'
-import { HTTPException } from 'hono/http-exception'
+import type { AppBindings } from "./types";
+import { logger } from "@helpers/logger";
+import { OpenAPIHono } from "@hono/zod-openapi";
+import { createErrorResponse } from "@utils/response";
+import { cors } from "hono/cors";
+import { HTTPException } from "hono/http-exception";
 
 export function createRouter() {
   return new OpenAPIHono<AppBindings>({
     strict: false,
     defaultHook: (result, c) => {
       if (!result.success) {
-        return c.json(createErrorResponse({
-          errors: result.error.format(),
-        }), 422)
+        return c.json(
+          createErrorResponse({
+            errors: result.error.format(),
+          }),
+          422
+        );
       }
     },
-  })
+  });
 }
 
 export function createApp() {
-  const app = createRouter()
+  const app = createRouter();
 
-  app.use(cors())
-  app.use(logger())
+  app.use(
+    "*",
+    cors({
+      origin: ["http://localhost:3000", "http://127.0.0.1:3000"],
+      allowMethods: ["POST", "GET", "PUT", "PATCH", "DELETE", "OPTIONS"],
+      credentials: true,
+    })
+  );
+  app.use(logger());
 
   app.notFound((c) => {
-    return c.json(createErrorResponse({ message: `Не найдено ${c.req.path}` }), 404)
-  })
+    return c.json(
+      createErrorResponse({ message: `Не найдено ${c.req.path}` }),
+      404
+    );
+  });
 
   app.onError((err, c) => {
     if (err instanceof HTTPException) {
-      const status = err.getResponse().status
+      const status = err.getResponse().status;
 
       if (status === 401) {
-        return c.json(createErrorResponse({ message: 'Unauthorized' }), status)
+        return c.json(createErrorResponse({ message: "Unauthorized" }), status);
       }
 
       if (status === 403) {
         return c.json(
-          createErrorResponse({ message: 'Недостаточно прав для действия' }),
-          status,
-        )
+          createErrorResponse({ message: "Недостаточно прав для действия" }),
+          status
+        );
       }
 
       if (status === 404) {
-        return c.json(createErrorResponse({ message: err.message }), status)
+        return c.json(createErrorResponse({ message: err.message }), status);
       }
     }
 
-    return c.json(createErrorResponse({ message: err.message }), 500)
-  })
+    return c.json(createErrorResponse({ message: err.message }), 500);
+  });
 
-  return app
+  return app;
 }
