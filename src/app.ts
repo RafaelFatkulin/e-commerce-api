aa20640a9b7c840555faff3204e2aca2742302f4
RafaelFatkulin <armistman@vk.com>
2025-01-27 18:01:03 +0500

    perm

--- src/app.ts
+++ src/app.ts
@@ -6,8 +6,18 @@ import { category } from '@modules/category'
 import { core } from '@modules/core'
 import { user } from '@modules/user'
 import { serveStatic } from 'hono/bun'
+import { cors } from 'hono/cors'
 
 export const app = createApp()
+
+app.use(
+  '*',
+  cors({
+    origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+    allowMethods: ['POST', 'GET', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    credentials: true,
+  }),
+)
 
 const routes = [
   core,
